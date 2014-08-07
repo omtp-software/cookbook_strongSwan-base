4a30bd148fd5022174cd431300bcca24cf1ec835
Jerry Jackson <schade@users.noreply.github.com>
2014-08-07 01:58:56 -0500

    Update metadata.rb
    adding the depend and recommend lines to add the cookbooks that have split out from this cookbook.

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,10 @@
 maintainer       "Jerry Jackson"
 maintainer_email "jerry.jackson@acommitteeoflunatics.com"
-license          "All rights reserved"
-description      "Installs/Configures strongswan"
+license          "All Rights Reserved"
+description      "Installs/Configures a strongSwan VPN"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
 depends 'route53'
+depends 'strongSwan-scenarios'
+recommends 'strongSwan-pki'
