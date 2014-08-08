545816c965c678916792c3675ad9a563a716bf4c
Jerry Jackson <jerry.jackson@acommitteeoflunatics.com>
2014-08-08 11:18:52 -0500

    Update metadata.rb
    Re-enable depends strongSwan-scenarios and recommends strongSwan-pki; removed route53 dependency.

--- metadata.rb
+++ metadata.rb
@@ -6,16 +6,13 @@ description      "Installs/Configures a strongSwan VPN"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
-depends 'route53'
 depends 'yum-epel'
-# depends 'strongSwan-scenarios'
+depends 'strongSwan-scenarios'
 
-# recommends 'strongSwan-pki'
+recommends 'strongSwan-pki'
 
-supports 'amazon'
-supports 'centos', ">= 6.0"
+supports 'arch'
 supports 'debian', ">= 7.0"
 supports 'fedora', ">= 19.0"
-supports 'oracle', ">= 6.0"
-supports 'redhat', ">= 6.0"
+supports 'rhel', ">= 6.0"
 supports 'ubuntu', ">= 14.04"
